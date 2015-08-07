c4357cb5d3e8b94e91c2418693a137df41aa2684
zhiyishou <liangzheng63@163.com>
2015-08-07 16:35:19 +0800

    add bad conblog.js

---
+++ core/server/storage/cnblog.js
@@ -0,0 +1,94 @@
+/**
+ * Created by liang on 2015/8/6.
+ */
+var _ = require("lodash"),
+    moment = require("moment"),
+    Promise = require("bluebird"),
+    request = Promise.promisify(require("request")),
+    parseString = require("xml2js").parseString,
+    fs = Promise.promisifyAll(require("fs")),
+    cronJob = require("cron").CronJob,
+    filePath = __dirname + "/cnblog.json";
+
+var cnblog = (function(){
+    var json;
+    return {
+        get:function(){
+            if(!json){
+                return fs.readFileAsync(filePath,"utf8")
+                    .then(JSON.parse)
+                    .done();
+                //updateCnblog(errorHandler);
+            }
+            return json;
+        },
+        set:function(j){
+            json = j;
+        }
+    }
+})();
+
+function updateCnblog (errorHandler){
+    return request("http://www.cnblogs.com/zhiyishou/rss")
+        .spread(function(res,body){
+            if(res.statusCode == 200){
+                return Promise.promisify(parseString)(body);
+            }else{
+                return Promise.reject("Http request fail!");
+            }
+        })
+        .then(function(result){
+            var tempPublished,
+                    tempObject,
+                newEntry = [];
+
+            _.map(result.feed.entry,function(entry){
+                var object = {
+                    title: entry.title[0]["_"].match(/.*(?=\s-)/)[0],
+                    url: entry.link[0]["$"].href
+                };
+
+                tempObject = _.last(newEntry);
+                tempPublished = moment(new Date(entry.published)).format("MMMM YYYY");
+                if(tempObject && tempObject.published == tempPublished){
+                    tempObject.entries.push(object);
+                }else{
+                    newEntry.push({
+                        published: tempPublished,
+                        entries: [object]
+                    });
+                }
+            });
+
+            return newEntry;
+        })
+        .then(function(result) {
+            cnblog.set(result);
+            return fs.writeFileAsync(filePath, JSON.stringify(result), "utf8");
+        })
+        .catch(errorHandler);
+}
+
+function errorHandler(){
+    console.log(e);
+    console.log("Will refetch again!");
+    new cronJob("0 0 0 * * *",doJob,null,true,"Asia/Shanghai")
+}
+
+function doJob(){
+    updateCnblog(errorHandler);
+}
+
+
+function cronUpdate(){
+    new cronJob("0 0 0 * * *",doJob,null,true,"Asia/Shanghai")
+}
+
+function init(){
+    cronUpdate();
+}
+
+exports.getCnblog = cnblog.get;
+exports.init = init;
+
+console.log(cnblog.get());
